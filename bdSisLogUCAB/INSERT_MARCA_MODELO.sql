43bb2976b2c8a03526909e821abd084bad443d82
uhnjel <angel.emo.altuve@gmail.com>
2018-12-11 21:06:04 -0400

    INSERTS anadidos
    insert de cliente, lugar, marca, modelo, sucursal, usuario, vehiculo, empleado, zona, cargo,rol

---
+++ bdSisLogUCAB/INSERT_MARCA_MODELO.sql
@@ -0,0 +1,24 @@
+insert into marca (nombre)values
+('Chevrolet'),
+('Hyundai'),
+('Ford'),
+('Volkswagen'),
+('Jeep'),
+('Chrysler'),
+('Dodge'),
+('Honda');
+('Mack');
+
+insert into modelo(nombre,fecha_prod,fk_marca)values
+('Corsa','02-03-2004',1),
+('Aveo','01-05-2010',1),
+('Elantra','03-04-2009',2),
+('Getz','03-06-2008',2),
+('Fiesta','07-08-2001',3),
+('Ka','02-01-2002',3),
+('Type 2','03-03-1970','4'),
+('Cherokee','12-10-2012','5'),
+('Wrangler','11-11-2005',5),
+('Voyager','10-5-2017',6),
+('Camaro','02-02-1999',7),
+('Civic','01-09-2003',8);

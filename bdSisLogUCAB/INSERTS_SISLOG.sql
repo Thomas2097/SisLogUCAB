d4e7b25476a6ac076397ca4603ed8a584cd8cedc
uhnjel <angel.emo.altuve@gmail.com>
2019-01-14 11:16:37 -0400

    reportes
    2

--- bdSisLogUCAB/INSERTS_SISLOG.sql
+++ bdSisLogUCAB/INSERTS_SISLOG.sql
@@ -10353,7 +10353,7 @@ insert into Aeropuerto (nombre, cant_term, cant_pistas, capacidad, fk_sucursal, 
 ('Aeropuerto Internacional Simon Bolivar', 17, 10, 30,299, 1346),
 ('Aeropuerto Internacional La Chinita', 8, 4, 5, 330, 1456),
 ('Aeropuerto Internacional Arturo Michelena', 15, 10, 6, 86, 649),
-('Aeropuerto Alberto Carnevali', 19, 8, 96, 189, 189),
+('Aeropuerto Alberto Carnevali', 19, 8, 96, 189, 879),
 ('Aeropuerto Internacional de Las Piedras Josefa Camejo', 17, 6, 54, 116,741);
 
 insert into Puerto(nombre, cant_puestos, cant_areas, longitud, ancho, calado, uso, fk_sucursal, fk_lugar) values 
